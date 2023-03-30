ff87314dfe73aa0c3c3130f1e40940e9e72f8e31
jomarderrie <deeznutsgottem6969@gmail.com>
2023-03-30 19:34:38 +0200

    add party

--- screens/parties/AddNewParty.js
+++ screens/parties/AddNewParty.js
@@ -1,7 +1,11 @@
-import {Container, Input, Text} from "native-base";
+import {Button, Container, Pressable, Text} from "native-base";
 import {ActivityIndicator, StyleSheet, View} from "react-native";
 import React, {useState} from "react";
 import FormContainer from "../shared/FormContainer";
+import DatePicker from "react-native-date-picker";
+import Input from "../shared/Input";
+import StyledButton from "../../styles/StyledButton";
+import Error from "../shared/Error";
 
 
 
@@ -9,7 +13,18 @@ import FormContainer from "../shared/FormContainer";
 const AddNewParty = () => {
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
-    const [startDate, setStartDate] = useState();
+    const [date, setDate] = useState(new Date())
+    const [open, setOpen] = useState(false)
+    const [err, setError] = useState();
+
+    const addParty = () => {
+        if (name === "" || date === "" || description === ""){
+            setError("Please fill in the form correctly")
+        }
+
+    }
+
+
     return(
       <FormContainer title={"Add new party"}>
           <View style={styles.label}>
@@ -32,6 +47,70 @@ const AddNewParty = () => {
               value={description}
               onChangeText={(text) => setDescription(text)}
           />
+          <Pressable onPress={() => setOpen(true)}>
+
+
+          <View style={styles.label} onPress={() => setOpen(true)}>
+              <Text style={{ textDecorationLine: "underline"}}>Party Date and time Click here</Text>
+          </View>
+          </Pressable>
+          <Text>Selected Date: {date ? date.toString(): "Date not selected"}</Text>
+          <DatePicker
+          modal
+          open={open}
+          date={date}
+          minimumDate={new Date()}
+          onConfirm={(date) => {
+              setOpen(false)
+              setDate(date)
+          }}
+          onCancel={() => {
+              setOpen(false)
+          }}
+      />
+          {err ? <Error message={err} /> : null}
+          <View style={styles.buttonContainer}>
+              <StyledButton
+                  large
+                  primary
+                  onPress={() => addParty()}
+              >
+                  <Text style={styles.buttonText}>Confirm</Text>
+              </StyledButton>
+          </View>
+          {/*<DatePicker*/}
+          {/*    date={date}*/}
+          {/*    mode="date"*/}
+          {/*    placeholder="select date"*/}
+          {/*    format="DD/MM/YYYY"*/}
+          {/*    minDate={new Date()}*/}
+          {/*    confirmBtnText="Confirm"*/}
+          {/*    cancelBtnText="Cancel"*/}
+          {/*    customStyles={{*/}
+          {/*        dateIcon: {*/}
+          {/*            position: 'absolute',*/}
+          {/*            right: -5,*/}
+          {/*            top: 4,*/}
+          {/*            marginLeft: 0,*/}
+          {/*        },*/}
+          {/*        dateInput: {*/}
+          {/*            borderColor : "gray",*/}
+          {/*            alignItems: "flex-start",*/}
+          {/*            borderWidth: 0,*/}
+          {/*            borderBottomWidth: 1,*/}
+          {/*        },*/}
+          {/*        placeholderText: {*/}
+          {/*            fontSize: 17,*/}
+          {/*            color: "gray"*/}
+          {/*        },*/}
+          {/*        dateText: {*/}
+          {/*            fontSize: 17,*/}
+          {/*        }*/}
+          {/*    }}*/}
+          {/*    onDateChange={(date) => {*/}
+          {/*        setDate(date);*/}
+          {/*    }}*/}
+          {/*/>*/}
 
       </FormContainer>
     )
